efca90430b8d5fc1e9dab538dae637344bda76e8
febriansyah  febriansyah <febripoetra65@gmail.com>
2018-11-02 22:31:01 +0700

    first

---
+++ koneksi.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "php-api";
+
+$db = mysqli_connect($servername, $username, $password, $dbname);
+if (!$db){
+        die("Connection Failed:".mysqli_connect_error());
+    }
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if (!$db){
+        die("Connection Failed:".mysqli_connect_error());
+    }	
+?>
